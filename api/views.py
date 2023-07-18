38424270373100ed2dd43218ec0b53eba19ccca7
shnurenkoviktoria <viktshnu-ipt27@lll.kpi.ua>
2023-07-18 03:27:35 +0300

    .

--- api/views.py
+++ api/views.py
@@ -10,7 +10,8 @@ class AuthorList(generics.ListAPIView):
     def get_queryset(self):
         queryset = Author.objects.all()
         name = self.request.query_params.get("name")
-        queryset = queryset.filter(name__icontains=name)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
 
         return queryset
 
