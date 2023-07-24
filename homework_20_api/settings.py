48932b89f42108c27c460c2ea49daae484ea192d
shnurenkoviktoria <viktshnu-ipt27@lll.kpi.ua>
2023-07-24 03:08:00 +0300

    docker and cache (hw22)

--- homework_20_api/settings.py
+++ homework_20_api/settings.py
@@ -98,6 +98,13 @@ if IS_HEROKU_APP:
 else:
     DATABASES = {
         "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+if "DOCKER_APP" in os.environ:
+    DATABASES = {
+        "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": "postgres",
             "USER": "postgres",
@@ -106,7 +113,6 @@ else:
             "PORT": "5432",
         }
     }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
