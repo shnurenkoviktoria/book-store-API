10578ee7c6d09cfd5c1021a8104725031b5f11c3
shnurenkoviktoria <viktshnu-ipt27@lll.kpi.ua>
2023-08-07 21:11:59 +0300

    mono fix (hw24)

--- api/mono.py
+++ api/mono.py
@@ -6,7 +6,7 @@ import ecdsa
 import requests
 from django.conf import settings
 from rest_framework.response import Response
-
+from rest_framework import status
 from api.models import Order, OrderItem
 
 
@@ -17,7 +17,7 @@ def create_order(order_data, webhook_url):
     order = Order.objects.create(total_price=0)
     for order_item in order_data:
         if order_item["quantity"] > order_item["book_id"].quantity:
-            return Response({"status": "Not enough books in stock"}, status=400)
+            return Response({"status": "Not enough books in stock"}, status=status.HTTP_400_BAD_REQUEST)
 
         else:
             order_item["book_id"].quantity -= order_item["quantity"]
