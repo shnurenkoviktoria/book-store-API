5896b6422621415e1b65efb5e1a181d5a44abf2d
shnurenkoviktoria <viktshnu-ipt27@lll.kpi.ua>
2023-08-07 22:01:03 +0300

    mono fix (hw24)

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api import views
 from api.views import OrderView, OrderCallbackView, OrdersViewSet
 
 urlpatterns = [
-    path("", views.urls_page, name="urls_page"),
+    path("urls_page/", views.urls_page, name="urls_page"),
     path("books/", views.BookList.as_view(), name="book-list"),
     path("books/<int:pk>/", views.BookDetail.as_view(), name="book-detail"),
     path("books/create/", views.BookCreate.as_view(), name="book-create"),
